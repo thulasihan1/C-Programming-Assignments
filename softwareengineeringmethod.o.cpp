30893d8dac67818780cacd4df4ff3c374de82d64
Thulasihan Sivarajah <39424972+thulasihan1@users.noreply.github.com>
2018-06-06 22:52:43 -0400

    Add files via upload

---
+++ softwareengineeringmethod.o.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(void)
+{
+	int i,j, r,c;
+	int *array;
+	scanf("%d %d",&r,&c);
+	array=(int*)calloc(r*c,sizeof(int));
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&array[i*c +j]);
+			
+											}
+										
+											}
+												
+												
+												
+												
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf(" %d ",&array[i*c +j]);
+			
+											}
+											
+											}
+	
+	
+				
+			free(array);
+			return(0);
+			
+											}
+

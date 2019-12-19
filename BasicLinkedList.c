2b9ea2c4f3a78b02266c87d773555fca43739ff5
Thulasihan Sivarajah <39424972+thulasihan1@users.noreply.github.com>
2019-12-19 19:55:16 +0000

    Add files via upload

---
+++ BasicLinkedList.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+	typedef	   struct lol{
+		
+		int value;
+	
+		struct lol *next;
+		
+		
+				
+					
+						
+							
+							} type;
+							
+
+		
+
+
+int main(void)
+{
+	
+	
+	type node1, node2, node3;
+	type *start;
+	start = &node1;
+	node1.next= &node2;
+	node2.next=&node3;
+	node3.next= NULL;
+	node1.value= 1000;
+	node2.value= 2000;
+	node3.value=3000;
+
+	printf("%d\n",start);
+						
+						
+						
+								}
+	
+		
+	
+		
+		
+			
+				
+					
+						
+							
+								
+									
+									
+										
+											
+												
+

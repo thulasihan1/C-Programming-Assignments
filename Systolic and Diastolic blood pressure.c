2a006533cce5aff759ac114868b1c4ec817e0c62
Thulasihan Sivarajah <39424972+thulasihan1@users.noreply.github.com>
2020-06-19 10:09:36 -0400

    Update Systolic and Diastolic blood pressure.c

--- Systolic and Diastolic blood pressure.c
+++ Systolic and Diastolic blood pressure.c
@@ -16,7 +16,9 @@ int main(void)
 		fscanf(in,"%d %d",&s,&d); /* Reading the systolic pressure first then the diastolic pressure from the file */
 		if(s>140 || d>190)        /* Ranges of systolic and diastolic pressures */
 		{
-			printf("WARNING!\n");   /*A Warning will appear if the systolic or diastolic pressure value is beyod their range */
+			printf("WARNING!\n");   /*A Warning will appear if the systolic or diastolic pressure value is beyod their range*/
+			printf("%d   %d\n", s, d);
+			
 								}
 								
 										}
